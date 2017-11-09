8a603d0e762293ff83d170c41584cb4bdcd8c956
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-11-09 14:53:45 +0100

    Added references and simple map display.

--- ProofOfConcept.Geodan/MainWindow.xaml.cs
+++ ProofOfConcept.Geodan/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BruTile.Predefined;
+using Mapsui.Geometries.WellKnownText;
+using Mapsui.Layers;
+using Newtonsoft.Json;
 
 namespace ProofOfConcept.Geodan
 {
@@ -23,6 +28,37 @@ namespace ProofOfConcept.Geodan
         public MainWindow()
         {
             InitializeComponent();
+
+            MyMapControl.Map.Layers.Add(new TileLayer(KnownTileSources.Create()));
+
+            searchButton.Click += OnSearch;
         }
-    }
+
+        private async void OnSearch(object sender, RoutedEventArgs e)
+        {
+            var searchText = searchTextBox.Text;
+
+            var data = FindAddress(searchText);
+
+            // WKT Parser
+            var geometry = GeometryFromWKT.Parse(data);
+
+            var a = geometry;
+        }
+
+        public string FindAddress(string query)
+        {
+            var url = $"https://services.geodan.nl/geosearch/free?q=" + query +
+                      "&q.op=OR&wt=json&start=0&rows=10&servicekey=19aca9fb-6705-11e7-a442-005056805b87";
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetStringAsync(url).Result;
+
+                dynamic json = JsonConvert.DeserializeObject(response);
+
+                return json.response.docs[0].geom;
+            }
+        }
+}
 }
