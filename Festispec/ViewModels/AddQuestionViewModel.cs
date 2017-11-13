a0015845699b67eae666dfca1c8928f24db72d73
ThomasSlot <tslot1@avans.nl>
2017-11-13 14:03:19 +0100

    add question to template

--- Festispec/ViewModels/AddQuestionViewModel.cs
+++ Festispec/ViewModels/AddQuestionViewModel.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Festispec.ViewModels.NavigationService;
+using Festispec.ViewModels.Template;
+using Festispec.ViewModels.Factory.Interface;
+using Festispec.Domain.Repository.Factory.Interface;
+using System.ComponentModel;
 
 namespace Festispec.ViewModels
 {
-    public class AddQuestionViewModel : NavigatableViewModelBase
+    public class AddQuestionViewModel : AddOrUpdateViewModelBase<ITemplateQuestionViewModelFactory, TemplateQuestionViewModel, Domain.TemplateQuestion>
     {
         
-        public AddQuestionViewModel(INavigationService navigationService) : base(navigationService)
+        public AddQuestionViewModel(INavigationService navigationService, ITemplateQuestionRepositoryFactory RepositoryFactory, ITemplateQuestionViewModelFactory TemplateVMFactory) : base(navigationService, RepositoryFactory, TemplateVMFactory)
         {
+        }
+
+        public override void OnNavigationServicePropertyChange(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName != "CurrentPageKey") return;
+
+            if (NavigationService.CurrentPageKey != Routes.Routes.AddQuestion.Key) return;
+
+            UpdateEntityViewModelFromNavigationParameter();
+        }
+
+        public override void Save()
+        {
+            NavigationService.GoBack(EntityViewModel);
         }
 
     }
