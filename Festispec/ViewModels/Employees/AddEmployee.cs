2bad9900e9ac592963fc3f90dffa44486fcbec9a
Ylja07 <yson@avans.com>
2017-11-15 12:33:13 +0100

    employee_crud
    small part of crud employee

---
+++ Festispec/ViewModels/Employees/AddEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Festispec.ViewModels.Employees
+{
+    class AddEmployee
+    {
+        public ICommand Add_Employee { get; set; }
+
+        public AddEmployee()
+        {
+            RegisterCommands();
+        }
+
+        private void RegisterCommands()
+        {
+            Add_Employee = new RelayCommand();
+        }
+    }
+}
---
+++ Festispec/Views/Crud_Employees/Crud_Employees.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Festispec.Views.CRUD_Employees
+{
+    /// <summary>
+    /// Interaction logic for Crud_Employees.xaml
+    /// </summary>
+    public partial class Crud_Employees : Window
+    {
+        public Crud_Employees()
+        {
+            InitializeComponent();
+        }
+    }
+}
