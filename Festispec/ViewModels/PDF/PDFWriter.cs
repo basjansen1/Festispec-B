085efc4cd8dc5182b0fe5a6247d3b52a7137aa53
ArjunAutar01 <asautar@avans.nl>
2017-11-16 14:50:06 +0100

    added PDFWriter base class

---
+++ Festispec/ViewModels/PDF/PDFWriter.cs
@@ -0,0 +1,19 @@
+﻿using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.ViewModels.PDF
+{
+    public abstract class PDFWriter
+    {
+        private PdfDocument document;
+        private string _title; 
+        public void SetTitle(string title)
+        {
+
+        }
+    }
+}
