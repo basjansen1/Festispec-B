3e01aecbd1db406163d8c7218983b0cdbee4ac94
ArjunAutar01 <asautar@avans.nl>
2017-11-16 14:57:38 +0100

    commit

--- Festispec/ViewModels/PDF/PDFWriter.cs
+++ Festispec/ViewModels/PDF/PDFWriter.cs
@@ -1,4 +1,5 @@
-﻿using PdfSharp.Pdf;
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,26 @@ namespace Festispec.ViewModels.PDF
     public abstract class PDFWriter
     {
         private PdfDocument document;
-        private string _title; 
+        private string _title;
+        private XFont _font;
+
+        public PDFWriter()
+        {
+            document = new PdfDocument();
+        }
         public void SetTitle(string title)
+        {
+            _font = new XFont("Verdana", 20, XFontStyle.Bold);
+        }
+
+        public void AddText(string text)
+        {
+
+        }
+
+        public void AddHeader() { }
+
+        public void ChangeFont()
         {
 
         }
