966fd5b990b1dff51fc42bd1fc75c24842c86d48
ArjunAutar01 <asautar@avans.nl>
2017-11-19 14:34:27 +0100

    implemented base for writing pdf's of documents

---
+++ Festispec.Domain/PDF/QuestionFormWriter.cs
@@ -0,0 +1,35 @@
+﻿using Festispec.ViewModels.PDF;
+using PdfSharp.Charting;
+using PdfSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.Domain.PDF
+{
+    public class QuestionFormWriter : PDFWriter
+    {
+        public void SetTitle(string title)
+        {
+            SetDocumentTitle(title);
+            AddEmptyLine();
+        }
+
+        public void AddParagraphHeader(string header)
+        {
+            AddParagraphTitle(header);
+        }
+
+        public void AddQuestion(string question)
+        {
+            AddLine(question, new XFont("Verdana", 12, XFontStyle.Bold));
+        }
+
+        public void AddAnswer(string answer)
+        {
+            AddLine(answer, new XFont("Verdana", 12, XFontStyle.Regular));
+        }
+    }
+}
