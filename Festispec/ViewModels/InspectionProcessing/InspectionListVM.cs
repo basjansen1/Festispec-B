5d5a0b1fe9c1721874c06b50dae2abdee3918ed2
Alex Mehlbaum <abjmehlb@avans.nl>
2017-11-21 15:40:13 +0100

    update4

--- Festispec/ViewModels/InspectionProcessing/InspectionListVM.cs
+++ Festispec/ViewModels/InspectionProcessing/InspectionListVM.cs
@@ -59,23 +59,25 @@ namespace Festispec.ViewModels.RequestProcessing
             ShowEditInspectionWindowCommand = new RelayCommand(ShowEditInspectionWindow);
             ShowProcessInspectionWindowCommand = new RelayCommand(ShowProcessInspectionWindow);
             DeleteInspectionCommand = new RelayCommand(DeleteSelectedInspection);
-
+            
             // instantiate views
-            _addInspectionView = new AddInspection();
-            _editInspectionView = new EditInspection();
-            _processInspectionView = new ProcessInspection();
+            
+            
+            
 
             using(var inspectionRepository = InspectionRepositoryFactory.CreateRepository())
             {
-                InspectionList = inspectionRepository.Get().Select(i => new InspectionVM(i)).ToList();
+            //    InspectionList = inspectionRepository.Get().Select(i => new InspectionVM(i)).ToList();
             }
 
-            InspectionVMList = new ObservableCollection<InspectionVM>(InspectionList);
+            // InspectionVMList = new ObservableCollection<InspectionVM>(InspectionList);
+            InspectionVMList = new ObservableCollection<InspectionVM>();
         }
 
         // methods
         public void ShowAddInspectionWindow()
         {
+            _addInspectionView = new AddInspection();
             _addInspectionView.Show();
         }
 
@@ -86,11 +88,13 @@ namespace Festispec.ViewModels.RequestProcessing
 
         public void ShowEditInspectionWindow()
         {
+            _editInspectionView = new EditInspection();
             _editInspectionView.Show();
         }
 
         public void ShowProcessInspectionWindow()
         {
+            _processInspectionView = new ProcessInspection();
             _processInspectionView.Show();
         }
 
