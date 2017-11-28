b3b44aaa9144093c754fc69a7110d79deb62df93
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-11-28 15:53:46 +0100

    Added tests

--- GeodanApi.Test/GeodanSearchApiUnitTest.cs
+++ GeodanApi.Test/GeodanSearchApiUnitTest.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace GeodanApi.Test
 {
-    [TestClass]
+    [TestFixture]
     public class GeodanSearchApiUnitTest
     {
-        [TestMethod]
+        [Test]
         public void BuildUrlValid()
         {
             // 1. Arrange
@@ -21,8 +21,7 @@ namespace GeodanApi.Test
                 "https://services.geodan.nl/geosearch/free?q=fpostcode:1079MB+AND+housenumber:1&servicekey=19aca9fb-6705-11e7-a442-005056805b87");
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
+        [Test]
         public void BuildUrlInvalidNoPostalCode()
         {
             // 1. Arrange
@@ -30,13 +29,12 @@ namespace GeodanApi.Test
             var geodanApiOptions = new GeodanApiOptions {HouseNumber = "1"};
 
             // 2. Act
-            var url = geodanSearchApi.BuildUrl(geodanApiOptions);
 
             // 3. Assert that an exception is thrown
+            Assert.Catch<ArgumentNullException>(() => geodanSearchApi.BuildUrl(geodanApiOptions));
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
+        [Test]
         public void BuildUrlInvalidNoHouseNumber()
         {
             // 1. Arrange
@@ -44,9 +42,34 @@ namespace GeodanApi.Test
             var geodanApiOptions = new GeodanApiOptions {PostalCode = "1079MB" };
 
             // 2. Act
-            var url = geodanSearchApi.BuildUrl(geodanApiOptions);
-            
+
             // 3. Assert that an exception is thrown
+            Assert.Catch<ArgumentNullException>(() => geodanSearchApi.BuildUrl(geodanApiOptions));
+        }
+
+        [Test]
+        public void FindValid()
+        {
+            // 1. Arrange
+            var geodanSearchApi = new GeodanSearchApi();
+            var geodanApiOptions = new GeodanApiOptions { PostalCode = "1079MB", HouseNumber = "1" };
+
+            // 2. Act
+            var data = geodanSearchApi.Find(geodanApiOptions);
+
+            // 3. Assert
+            Assert.AreEqual(data.street.ToString(), "President Kennedylaan");
+            Assert.AreEqual(data.municipality.ToString(), "Amsterdam");
+            Assert.AreEqual(data.type.ToString(), "address");
+            Assert.AreEqual(data.city.ToString(), "Amsterdam");
+            Assert.AreEqual(data.id.ToString(), "address-0363100012081613-0363200000241158");
+            Assert.AreEqual(data.naturalid.ToString(), "0363200000241158");
+            Assert.AreEqual(data.province.ToString(), "Noord-Holland");
+            Assert.AreEqual(data.housenumber.ToString(), "1");
+            Assert.AreEqual(data.postcode.ToString(), "1079MB");
+            Assert.AreEqual(data.geom.ToString(), "POINT(4.91311 52.34232)");
+            Assert.AreEqual(data.geom_rd.ToString(), "POINT(122692 483928)");
+            Assert.AreEqual(data.displayname.ToString(), "President Kennedylaan 1, Amsterdam");
         }
     }
 }
