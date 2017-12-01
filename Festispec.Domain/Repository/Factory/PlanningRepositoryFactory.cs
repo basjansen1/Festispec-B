d34410fdd2207a9def8dcf2f671b8c97fbf57ab8
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-01 09:19:23 +0100

    Created planning repository

---
+++ Festispec.Domain/Repository/Factory/PlanningRepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using Festispec.Domain.Repository.Factory.Interface;
+using Festispec.Domain.Repository.Interface;
+
+namespace Festispec.Domain.Repository.Factory
+{
+    public class PlanningRepositoryFactory : RepositoryFactoryBase<IPlanningRepository, Planning>,
+        IPlanningRepositoryFactory
+    {
+        public override IPlanningRepository CreateRepository()
+        {
+            return new PlanningRepository(GetDbContext());
+        }
+    }
+}
