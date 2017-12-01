021aa168828ea2f78a35a13c38782fcb8bb454fd
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-01 17:45:41 +0100

    Updated repository

--- Festispec.Domain/Repository/Interface/IPlanningRepository.cs
+++ Festispec.Domain/Repository/Interface/IPlanningRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace Festispec.Domain.Repository.Interface
+﻿using System.Linq;
+
+namespace Festispec.Domain.Repository.Interface
 {
     public interface IPlanningRepository : IRepository<Planning>
     {
+        IQueryable<Planning> GetByInspectionId(int inspectionId);
     }
 }
--- Festispec.Domain/Repository/PlanningRepository.cs
+++ Festispec.Domain/Repository/PlanningRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Linq;
 using Festispec.Domain.Repository.Interface;
 
 namespace Festispec.Domain.Repository
@@ -7,6 +8,16 @@ namespace Festispec.Domain.Repository
     {
         public PlanningRepository(DbContext dbContext) : base(dbContext)
         {
+        }
+
+        public override IQueryable<Planning> Get()
+        {
+            return base.Get().Include(planning => planning.Inspector).Include(planning => planning.Inspection);
+        }
+
+        public IQueryable<Planning> GetByInspectionId(int inspectionId)
+        {
+            return Get().Where(planning => planning.Inspection_Id == inspectionId);
         }
     }
 }
