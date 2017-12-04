10aa8416e90a219d47bf0a4a87b675819da35aad
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-04 13:39:17 +0100

    Fixed database

--- Festispec.Domain/Repository/InspectionRepository.cs
+++ Festispec.Domain/Repository/InspectionRepository.cs
@@ -14,5 +14,30 @@ namespace Festispec.Domain.Repository
             return base.Get().Include(inspection => inspection.Customers);
         }
 
+        public override Inspection Add(Inspection entity)
+        {
+            entity = CleanRelations(entity);
+            return base.Add(entity);
+        }
+
+
+        public override Inspection Update(Inspection updated, params object[] keyValues)
+        {
+            updated = CleanRelations(updated);
+            return base.Update(updated, keyValues);
+        }
+
+        private static Inspection CleanRelations(Inspection entity)
+        {
+            if (entity.Customers != null)
+            {
+                // Set foreign key and unset the navigation property
+                // This is needed because we are working with disconnected entities.
+                entity.Customer_Id = entity.Customers.Id;
+                entity.Customers = null;
+            }
+
+            return entity;
+        }
     }
 }
