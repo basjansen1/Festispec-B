0a223ddf462802eb78ecdf32ddfea767e1d61c66
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-04 13:31:26 +0100

    Fixed database merge

--- Festispec.Database/dbo/Tables/Address_Inspection.sql
+++ Festispec.Database/dbo/Tables/Address_Inspection.sql
@@ -3,9 +3,11 @@
     [Website]       NVARCHAR (MAX) NULL,
     [Start]         DATETIME       NOT NULL,
     [End]           DATETIME       NOT NULL,
-    [Id]            INT            NOT NULL,
     [Status_Status] NVARCHAR (128) NOT NULL,
+    [Customer_Id]   INT            NOT NULL,
+    [Id]            INT            NOT NULL,
     CONSTRAINT [PK_Address_Inspection] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_CustomerInspection] FOREIGN KEY ([Customer_Id]) REFERENCES [dbo].[Address_Customer] ([Id]),
     CONSTRAINT [FK_Inspection_inherits_Address] FOREIGN KEY ([Id]) REFERENCES [dbo].[Address] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_InspectionStatusInspection] FOREIGN KEY ([Status_Status]) REFERENCES [dbo].[InspectionStatus] ([Status])
 );
@@ -14,4 +16,9 @@
 GO
 CREATE NONCLUSTERED INDEX [IX_FK_InspectionStatusInspection]
     ON [dbo].[Address_Inspection]([Status_Status] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_FK_CustomerInspection]
+    ON [dbo].[Address_Inspection]([Customer_Id] ASC);
 
--- Festispec.Database/dbo/Tables/Question_InspectionQuestion.sql
+++ Festispec.Database/dbo/Tables/Question_InspectionQuestion.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Question_InspectionQuestion] (
     [Answer]                 NVARCHAR (MAX) NULL,
-    [Id]                     INT            NOT NULL,
     [Planning_Inspection_Id] INT            NOT NULL,
     [Planning_Inspector_Id]  INT            NOT NULL,
     [Planning_Date]          DATETIME       NOT NULL,
+    [Id]                     INT            NOT NULL,
     CONSTRAINT [PK_Question_InspectionQuestion] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_InspectionQuestion_inherits_Question] FOREIGN KEY ([Id]) REFERENCES [dbo].[Question] ([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_PlanningInspectionQuestion] FOREIGN KEY ([Planning_Inspection_Id], [Planning_Inspector_Id], [Planning_Date]) REFERENCES [dbo].[Planning] ([Inspection_Id], [Inspector_Id], [Date])
