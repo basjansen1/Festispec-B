c910e83a916537537900b2a45de29a19d5efda64
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-09 15:27:06 +0100

    Fixed template delete

--- Festispec.Database/dbo/Tables/Question_TemplateQuestion.sql
+++ Festispec.Database/dbo/Tables/Question_TemplateQuestion.sql
@@ -3,7 +3,7 @@
     [Id]          INT NOT NULL,
     CONSTRAINT [PK_Question_TemplateQuestion] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_TemplateQuestion_inherits_Question] FOREIGN KEY ([Id]) REFERENCES [dbo].[Question] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_TemplateTemplateQuestion] FOREIGN KEY ([Template_Id]) REFERENCES [dbo].[Template] ([Id])
+    CONSTRAINT [FK_TemplateTemplateQuestion] FOREIGN KEY ([Template_Id]) REFERENCES [dbo].[Template] ([Id]) ON DELETE CASCADE
 );
 
 
