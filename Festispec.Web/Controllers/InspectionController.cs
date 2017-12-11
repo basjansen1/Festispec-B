6fe6683f44bc57676abde0ecf023a695b9814aa3
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-11 15:41:24 +0100

    Added inspection controller

---
+++ Festispec.Web/Controllers/InspectionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Festispec.Web.Controllers
+{
+    public class InspectionController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Inspecties.";
+
+            return View();
+        }
+        public ActionResult Inspect(int inspectionId)
+        {
+            ViewBag.Message = "Inspectie uitvoeren.";
+
+            return View();
+        }
+    }
+}
