d751ee5ec5c2aab83d9a676201280c7bf822e275
Sietse Manders <shjmanders@hotmail.com>
2017-12-11 13:28:31 +0100

    Changed language

--- Festispec/ViewModels/LoginViewModel.cs
+++ Festispec/ViewModels/LoginViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Festispec.Domain.Repository.Factory.Interface;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System;
 using Festispec.State;
 
 namespace Festispec.ViewModels
