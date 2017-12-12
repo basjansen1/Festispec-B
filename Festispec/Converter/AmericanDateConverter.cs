8a0e78e8d81222a0000670576ddc084453639146
Sietse Manders <shjmanders@hotmail.com>
2017-12-12 19:19:03 +0100

    Work in progress DateConverter

---
+++ Festispec/Converter/AmericanDateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Festispec.Converter
+{
+    public class AmericanDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            DateTime dateTime = (DateTime) value;
+            return dateTime.ToString("dd MMMM yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
