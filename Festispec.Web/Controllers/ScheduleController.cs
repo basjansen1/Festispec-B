aedb4f0eff9dc2e1a1d8f03ae993ddee7a0514c8
ThomasSlot <tslot1@avans.nl>
2017-12-13 14:48:41 +0100

    start inspectionSchedule

---
+++ Festispec.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,41 @@
+﻿using Festispec.Domain.Repository.Factory;
+using Festispec.Domain.Repository.Factory.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Festispec.Web.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private readonly IInspectorScheduleRepositoryFactory _inspectorScheduleRepositoryFactory;
+
+        public ScheduleController()
+        {
+            _inspectorScheduleRepositoryFactory = new InspectorScheduleRepositoryFactory(true);
+        }
+
+        //copy pasta inspectioncontroller
+
+        //public ActionResult Index()
+        //{
+        //    ViewBag.Message = "Inspecties.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Inspect(int inspectionId)
+        //{
+        //    ViewBag.Message = "Inspectie uitvoeren.";
+
+        //    using (var inspectionRepository = _inspectionRepositoryFactory.CreateRepository())
+        //    {
+        //        var inspection = inspectionRepository.Get(inspectionId);
+
+        //        return View(inspection);
+        //    }
+        //}
+    }
+}
