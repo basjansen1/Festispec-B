5e2f7ef16c5fff2d191b305a6bfaa23dc21286ac
Sven Slijkoord <sven.slijkoord@gmail.com>
2017-12-16 14:42:02 +0100

    More cleaning

--- Festispec.Web/Controllers/HomeController.cs
+++ Festispec.Web/Controllers/HomeController.cs
@@ -13,12 +13,5 @@ namespace Festispec.Web.Controllers
         {
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
