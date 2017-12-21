fe4325319bc8b9c95a22484272cd1b1595c5e05f
Martijn Knook <mknook@avans.nl>
2017-12-21 12:38:53 +0100

    Changess to charts

--- Festispec.Domain/PDF/ChartExporter.cs
+++ Festispec.Domain/PDF/ChartExporter.cs
@@ -13,28 +13,30 @@ namespace Festispec.Domain
         {
             Chart chart1 = new Chart();
             chart1.Titles.Add(title);
+            chart1.ChartAreas.Add(new ChartArea("Area")
+            {
+                BackColor = Color.WhiteSmoke,
+                Area3DStyle = { Enable3D = false },
+            });
             for (int groupNumber = 0; groupNumber < group.Count; groupNumber++)
             {
                 chart1.Series.Add(group[groupNumber]);
                 chart1.Series[group[groupNumber]].ChartType = SeriesChartType.Column;
                 chart1.Series[group[groupNumber]].BorderWidth = 1;
-                
-                    for (int yxNumber = 0; yxNumber < x.Count; yxNumber++)
+                chart1.Legends.Add(new Legend(group[groupNumber]));
+                chart1.Legends[group[groupNumber]].DockedToChartArea = "Area";
+                chart1.Series[group[groupNumber]].Legend = group[groupNumber];
+                chart1.Series[group[groupNumber]].IsVisibleInLegend = true;
+                for (int yxNumber = 0; yxNumber < x.Count; yxNumber++)
+                {
+                    for (int yNumber = 0; yNumber < y[yxNumber].Count; yNumber++)
                     {
-                        for (int yNumber = 0; yNumber < y[yxNumber].Count; yNumber++)
-                        {
-                            chart1.Series[group[groupNumber]].Points.AddXY(x[yxNumber], y[yxNumber][yNumber]);
-                        }
+                        chart1.Series[group[groupNumber]].Points.AddXY(x[yxNumber], y[yxNumber][yNumber]);
                     }
-                
+                }
+
             }
-            chart1.Size = new Size(500,140);
-            chart1.ChartAreas.Add(new ChartArea("Area")
-            {
-                BackColor = Color.WhiteSmoke,
-                Area3DStyle = { Enable3D = false },
-                
-            });
+            chart1.Size = new Size(500, 140);
             chart1.Enabled = true;
             chart1.DataBind();
             MemoryStream memoryStream = new MemoryStream();
@@ -47,22 +49,26 @@ namespace Festispec.Domain
         {
             Chart chart1 = new Chart();
             chart1.Titles.Add(title);
+            chart1.ChartAreas.Add(new ChartArea("Area")
+            {
+                BackColor = Color.WhiteSmoke,
+                Area3DStyle = { Enable3D = false },
+            });
             for (int inspectionNumber = 0; inspectionNumber < x.Count; inspectionNumber++)
             {
                 chart1.Series.Add(x[inspectionNumber]);
                 chart1.Series[x[inspectionNumber]].ChartType = SeriesChartType.Column;
                 chart1.Series[x[inspectionNumber]].BorderWidth = 1;
                 chart1.Series[x[inspectionNumber]].BorderColor = Color.White;
+                chart1.Legends.Add(new Legend(x[inspectionNumber]));
+                chart1.Legends[x[inspectionNumber]].DockedToChartArea = "Area";
+                chart1.Series[x[inspectionNumber]].Legend = x[inspectionNumber];
+                chart1.Series[x[inspectionNumber]].IsVisibleInLegend = true;
                 for (int dataNumber = 0; dataNumber < y[inspectionNumber].Count; dataNumber++)
                 {
-                    chart1.Series[x[inspectionNumber]].Points.AddXY(x[inspectionNumber], y[inspectionNumber][dataNumber]);
+                    chart1.Series[x[inspectionNumber]].Points.AddXY(1, y[inspectionNumber][dataNumber]);
                 }
             }
-            chart1.ChartAreas.Add(new ChartArea("Area")
-            {
-                BackColor = Color.WhiteSmoke,
-                Area3DStyle = { Enable3D = false },
-            });
             chart1.Enabled = true;
             chart1.DataBind();
             MemoryStream memoryStream = new MemoryStream();
