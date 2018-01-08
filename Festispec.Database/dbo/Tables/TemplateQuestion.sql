0a6f13a7496dfd3bc899b639bbd6dca2aac73760
Sven Slijkoord <sven.slijkoord@gmail.com>
2018-01-08 10:37:11 +0100

    Fixed delete inspection template

--- Festispec.Database/dbo/Tables/TemplateQuestion.sql
+++ Festispec.Database/dbo/Tables/TemplateQuestion.sql
@@ -3,7 +3,7 @@
     [Question_Id] INT NOT NULL,
     CONSTRAINT [PK_TemplateQuestion] PRIMARY KEY CLUSTERED ([Template_Id] ASC, [Question_Id] ASC),
     CONSTRAINT [FK_QuestionTemplateQuestion] FOREIGN KEY ([Question_Id]) REFERENCES [dbo].[Question] ([Id]),
-    CONSTRAINT [FK_TemplateTemplateQuestion] FOREIGN KEY ([Template_Id]) REFERENCES [dbo].[Template] ([Id])
+    CONSTRAINT [FK_TemplateTemplateQuestion] FOREIGN KEY ([Template_Id]) REFERENCES [dbo].[Template] ([Id]) ON DELETE CASCADE
 );
 
 
