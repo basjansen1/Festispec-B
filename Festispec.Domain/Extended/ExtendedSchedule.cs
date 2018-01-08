7dd38a61e661cb0baa8faad9054780aa6cb8b752
ThomasSlot <tslot1@avans.nl>
2018-01-08 12:59:29 +0100

    hij doet t

--- Festispec.Domain/Extended/ExtendedSchedule.cs
+++ Festispec.Domain/Extended/ExtendedSchedule.cs
@@ -1,13 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.Collections.Generic;
 
 namespace Festispec.Domain
 {
-    public partial class Schedule
+    public partial class Schedule : IValidatableObject
     {
         [NotMapped]
         public bool IsDeleted { get; set; } = false;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+            DateTime from = new DateTime(1970, 1, 1);
+
+            if (NotAvailableFrom < from)
+            {
+                results.Add(new ValidationResult("ERROR: Minimaal 1/1/1970", new[] { "NotAvailableFrom" }));
+            }
+
+            if (NotAvailableTo < NotAvailableFrom)
+            {
+                results.Add(new ValidationResult("ERROR: Niet beschikbaar tot mag niet eerder zijn dan Niet beschikbaar vanaf", new[] { "NotAvailableTo" }));
+            }
+
+            return results;
+        }
 
         public sealed class ScheduleAttributes
         {
@@ -17,13 +36,11 @@ namespace Festispec.Domain
             public DateTime NotAvailableTo { get; set; }
 
             [Required]
-            [DataType(DataType.Date, ErrorMessage = "Datum groter dan x-x-1990")]
-            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            [Range(typeof(DateTime), "1/1/1970", "1/1/2100")]
             public DateTime NotAvailableDateFrom { get; set; }
 
             [Required]
-            [DataType(DataType.Date, ErrorMessage = "Datum groter dan x-x-1990")]
-            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            [Range(typeof(DateTime), "1/1/1970", "1/1/2100")]
             public DateTime NotAvailableDateTo { get; set; }
         }
     }
